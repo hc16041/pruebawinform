bd9edf03a1530ca457fa4a73c8d87e78c42c68fc
Fernando Castellanos <hc16041@ues.edu.sv>
2023-04-22 00:08:16 -0600

    Agregar archivos de proyecto.

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,193 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.DGVUSUARIO = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtUsu = New System.Windows.Forms.TextBox()
+        Me.txtCon = New System.Windows.Forms.TextBox()
+        Me.numInt = New System.Windows.Forms.NumericUpDown()
+        Me.numNiv = New System.Windows.Forms.NumericUpDown()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.btnEditar = New System.Windows.Forms.Button()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        CType(Me.DGVUSUARIO, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.numInt, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.numNiv, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(549, 239)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Conectar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'DGVUSUARIO
+        '
+        Me.DGVUSUARIO.AllowUserToOrderColumns = True
+        Me.DGVUSUARIO.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DGVUSUARIO.Location = New System.Drawing.Point(42, 239)
+        Me.DGVUSUARIO.Name = "DGVUSUARIO"
+        Me.DGVUSUARIO.Size = New System.Drawing.Size(484, 177)
+        Me.DGVUSUARIO.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(42, 85)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Usuario"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(42, 113)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(38, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Contra"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(42, 139)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(44, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "intentos"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(42, 164)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(29, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "nivel"
+        '
+        'txtUsu
+        '
+        Me.txtUsu.Location = New System.Drawing.Point(91, 82)
+        Me.txtUsu.Name = "txtUsu"
+        Me.txtUsu.Size = New System.Drawing.Size(100, 20)
+        Me.txtUsu.TabIndex = 6
+        '
+        'txtCon
+        '
+        Me.txtCon.Location = New System.Drawing.Point(91, 108)
+        Me.txtCon.Name = "txtCon"
+        Me.txtCon.Size = New System.Drawing.Size(100, 20)
+        Me.txtCon.TabIndex = 7
+        '
+        'numInt
+        '
+        Me.numInt.Location = New System.Drawing.Point(91, 137)
+        Me.numInt.Name = "numInt"
+        Me.numInt.Size = New System.Drawing.Size(37, 20)
+        Me.numInt.TabIndex = 8
+        '
+        'numNiv
+        '
+        Me.numNiv.Location = New System.Drawing.Point(91, 164)
+        Me.numNiv.Name = "numNiv"
+        Me.numNiv.Size = New System.Drawing.Size(37, 20)
+        Me.numNiv.TabIndex = 9
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.Location = New System.Drawing.Point(267, 74)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(75, 23)
+        Me.btnGuardar.TabIndex = 10
+        Me.btnGuardar.Text = "Guardar"
+        Me.btnGuardar.UseVisualStyleBackColor = True
+        '
+        'btnEditar
+        '
+        Me.btnEditar.Location = New System.Drawing.Point(267, 108)
+        Me.btnEditar.Name = "btnEditar"
+        Me.btnEditar.Size = New System.Drawing.Size(75, 23)
+        Me.btnEditar.TabIndex = 11
+        Me.btnEditar.Text = "Actualizar"
+        Me.btnEditar.UseVisualStyleBackColor = True
+        '
+        'btnEliminar
+        '
+        Me.btnEliminar.Location = New System.Drawing.Point(267, 139)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(75, 23)
+        Me.btnEliminar.TabIndex = 12
+        Me.btnEliminar.Text = "Eliminar"
+        Me.btnEliminar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnEliminar)
+        Me.Controls.Add(Me.btnEditar)
+        Me.Controls.Add(Me.btnGuardar)
+        Me.Controls.Add(Me.numNiv)
+        Me.Controls.Add(Me.numInt)
+        Me.Controls.Add(Me.txtCon)
+        Me.Controls.Add(Me.txtUsu)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DGVUSUARIO)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DGVUSUARIO, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.numInt, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.numNiv, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents DGVUSUARIO As DataGridView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtUsu As TextBox
+    Friend WithEvents txtCon As TextBox
+    Friend WithEvents numInt As NumericUpDown
+    Friend WithEvents numNiv As NumericUpDown
+    Friend WithEvents btnGuardar As Button
+    Friend WithEvents btnEditar As Button
+    Friend WithEvents btnEliminar As Button
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,291 @@
+﻿Imports System.Data.SqlClient
+Imports System.Configuration
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        'CADENA DE CONEXION
+        Dim connectionString As String = "Data source=DESKTOP-B1JIUS0\SQLEXPRESS02; Initial Catalog=Prueba01;Integrated Security=True"
+
+        ' Crear una nueva conexión a SQL Server
+        Dim connection As New SqlConnection(connectionString)
+
+        Try
+            ' Abrir la conexión
+            connection.Open()
+            ' La conexión se ha abierto exitosamente, mostrar un mensaje de éxito
+            'MessageBox.Show("Conexión exitosa a la base de datos.")
+
+            'LUEGO CREAR UN COMANDO PARA PODER EJECUTAR COMANDOS
+            Dim comando As New SqlCommand("SP_MTTO_USUARIO", connection)
+
+            'AGREGAR DE QUE TIPO ES EL COMANDO
+            comando.CommandType = CommandType.StoredProcedure
+
+            'AGREGAR LOS VALORES A ENVIAR AL SP
+            comando.Parameters.AddWithValue("@opcion", "0")
+
+            'EJECUTAR UN ADAPTADOR
+            Dim adaptador As New SqlDataAdapter(comando)
+
+            'GUARDAR EN UNA TABLA
+            Dim tabla As New DataTable
+            adaptador.Fill(tabla)
+
+            DGVUSUARIO.DataSource = tabla
+
+            'MOSTRAMOS UN VALOR
+            'MessageBox.Show(Convert.ToString(tabla.Rows(0)(1)))
+
+            ' Cerrar la conexión
+            connection.Close()
+        Catch ex As Exception
+            ' Manejar errores de conexión
+            MessageBox.Show("Error al conectar a la base de datos: " + ex.Message)
+        Finally
+            ' Asegurarse de cerrar la conexión al salir del bloque Try-Catch
+            connection.Close()
+        End Try
+
+    End Sub
+
+
+    Private Sub DGVUSUARIO_MouseClick(sender As Object, e As MouseEventArgs) Handles DGVUSUARIO.MouseClick, Button1.MouseClick
+        Dim rowIndex As Integer = DGVUSUARIO.CurrentRow.Index
+
+        If rowIndex <> -1 Then
+            'obtenemos datos
+            Dim id As String = DGVUSUARIO.Rows(rowIndex).Cells(0).Value.ToString()
+            Dim usuario As String = DGVUSUARIO.Rows(rowIndex).Cells(1).Value.ToString()
+            Dim contra As String = DGVUSUARIO.Rows(rowIndex).Cells(2).Value.ToString()
+            Dim intentos As String = DGVUSUARIO.Rows(rowIndex).Cells(3).Value.ToString()
+            Dim nivel As String = DGVUSUARIO.Rows(rowIndex).Cells(4).Value.ToString()
+
+            If ((String.IsNullOrWhiteSpace(id) And String.IsNullOrWhiteSpace(usuario) And String.IsNullOrWhiteSpace(contra) And String.IsNullOrWhiteSpace(intentos) And String.IsNullOrWhiteSpace(nivel)) = False) Then
+                'mostramos datos
+                txtUsu.Text = usuario
+                txtCon.Text = contra
+                numInt.Value = Convert.ToDecimal(intentos)
+                numNiv.Value = Convert.ToDecimal(nivel)
+                btnGuardar.Enabled = False
+                btnEditar.Enabled = True
+                btnEliminar.Enabled = True
+            Else Limpiar()
+            End If
+        End If
+
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'CADENA DE CONEXION
+        Dim connectionString As String = "Data source=DESKTOP-B1JIUS0\SQLEXPRESS02; Initial Catalog=Prueba01;Integrated Security=True"
+        ' Crear una nueva conexión a SQL Server
+        'Dim connection As New SqlConnection(connectionString)
+
+        Mostrar(connectionString)
+    End Sub
+
+    Private Sub Mostrar(ByVal con As String)
+        Limpiar()
+        'CADENA DE CONEXION
+        'Dim connectionString As String = "Data source=DESKTOP-B1JIUS0\SQLEXPRESS02; Initial Catalog=Prueba01;Integrated Security=True"
+        ' Crear una nueva conexión a SQL Server
+        Dim connection As New SqlConnection(con)
+
+        Dim comando As New SqlCommand("SP_MTTO_USUARIO", connection)
+
+        'AGREGAR DE QUE TIPO ES EL COMANDO
+        comando.CommandType = CommandType.StoredProcedure
+
+        'AGREGAR LOS VALORES A ENVIAR AL SP
+        comando.Parameters.AddWithValue("@opcion", "0")
+
+        'EJECUTAR UN ADAPTADOR
+        Dim adaptador As New SqlDataAdapter(comando)
+
+        'GUARDAR EN UNA TABLA
+        Dim tabla As New DataTable
+        adaptador.Fill(tabla)
+
+        DGVUSUARIO.DataSource = tabla
+    End Sub
+
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        'CADENA DE CONEXION
+        Dim connectionString As String = "Data source=DESKTOP-B1JIUS0\SQLEXPRESS02; Initial Catalog=Prueba01;Integrated Security=True"
+        ' Crear una nueva conexión a SQL Server
+        'Dim connection As New SqlConnection(connectionString)
+        If txtUsu.Text = "" Then
+            MessageBox.Show("Ingresar usuario")
+            Return
+        ElseIf txtCon.Text = "" Then
+            MessageBox.Show("Ingresar contrasenia")
+            Return
+        ElseIf numInt.Value <= 0 Then
+            MessageBox.Show("El numero de intentos debe de ser diferente y mayor a 0")
+            Return
+        ElseIf numNiv.Value <= 0 Then
+            MessageBox.Show("El numero de nivel debe de ser diferente y mayor a 0")
+            Return
+        Else Guardar(connectionString, txtUsu.Text, txtCon.Text, numInt.Value.ToString(), numNiv.Value.ToString())
+        End If
+
+
+
+    End Sub
+
+    Private Sub btnEditar_Click(sender As Object, e As EventArgs) Handles btnEditar.Click
+        'CADENA DE CONEXION
+        Dim connectionString As String = "Data source=DESKTOP-B1JIUS0\SQLEXPRESS02; Initial Catalog=Prueba01;Integrated Security=True"
+        ' Crear una nueva conexión a SQL Server
+        'Dim connection As New SqlConnection(connectionString)
+        Dim VALOR As String = DGVUSUARIO.Rows(DGVUSUARIO.CurrentRow.Index).Cells(0).Value.ToString()
+        If String.IsNullOrWhiteSpace(VALOR) Then
+            MessageBox.Show("Seleccionar una fila que no este vacia")
+            Return
+        ElseIf txtUsu.Text = "" Then
+        MessageBox.Show("Ingresar usuario")
+            Return
+        ElseIf txtCon.Text = "" Then
+            MessageBox.Show("Ingresar contrasenia")
+            Return
+        ElseIf numInt.Value <= 0 Then
+            MessageBox.Show("El numero de intentos debe de ser diferente y mayor a 0")
+            Return
+        ElseIf numNiv.Value <= 0 Then
+            MessageBox.Show("El numero de nivel debe de ser diferente y mayor a 0")
+            Return
+        Else Actualizar(connectionString, txtUsu.Text, txtCon.Text, numInt.Value.ToString(), numNiv.Value.ToString(), VALOR)
+        End If
+
+    End Sub
+
+    Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+        'CADENA DE CONEXION
+        Dim connectionString As String = "Data source=DESKTOP-B1JIUS0\SQLEXPRESS02; Initial Catalog=Prueba01;Integrated Security=True"
+        ' Crear una nueva conexión a SQL Server
+        'Dim connection As New SqlConnection(connectionString)
+        Dim VALOR As String = DGVUSUARIO.Rows(DGVUSUARIO.CurrentRow.Index).Cells(0).Value.ToString()
+        If String.IsNullOrWhiteSpace(VALOR) Then
+            MessageBox.Show("Seleccionar una fila que no este vacia")
+            Return
+        Else Eliminar(connectionString, VALOR)
+        End If
+
+    End Sub
+
+    Private Sub Guardar(ByVal con As String, ByVal usu As String, ByVal contra As String, ByVal inte As String, ByVal niv As String)
+
+        'CADENA DE CONEXION
+        'Dim connectionString As String = "Data source=DESKTOP-B1JIUS0\SQLEXPRESS02; Initial Catalog=Prueba01;Integrated Security=True"
+        ' Crear una nueva conexión a SQL Server
+        Dim connection As New SqlConnection(con)
+
+        Dim comando As New SqlCommand("SP_MTTO_USUARIO", connection)
+
+        'AGREGAR DE QUE TIPO ES EL COMANDO
+        comando.CommandType = CommandType.StoredProcedure
+
+        'AGREGAR LOS VALORES A ENVIAR AL SP
+        comando.Parameters.AddWithValue("@opcion", "1")
+        comando.Parameters.AddWithValue("@usuario", usu)
+        comando.Parameters.AddWithValue("@contrasena", contra)
+        comando.Parameters.AddWithValue("@intentos", inte)
+        comando.Parameters.AddWithValue("@nivelSeg", niv)
+
+        'EJECUTAR UN ADAPTADOR
+        Dim adaptador As New SqlDataAdapter(comando)
+
+        'GUARDAR EN UNA TABLA
+        Dim tabla As New DataTable
+        adaptador.Fill(tabla)
+
+
+        If tabla.Rows(0)(0).ToString() = "1" Then
+            MessageBox.Show("El dato se guardo correctamente")
+            Mostrar(con)
+        Else MessageBox.Show("El dato no se guardo correctamente")
+        End If
+
+        'DGVUSUARIO.DataSource = tabla
+    End Sub
+
+    Private Sub Actualizar(ByVal con As String, ByVal usu As String, ByVal contra As String, ByVal inte As String, ByVal niv As String, ByVal id As String)
+
+        'CADENA DE CONEXION
+        'Dim connectionString As String = "Data source=DESKTOP-B1JIUS0\SQLEXPRESS02; Initial Catalog=Prueba01;Integrated Security=True"
+        ' Crear una nueva conexión a SQL Server
+        Dim connection As New SqlConnection(con)
+
+        Dim comando As New SqlCommand("SP_MTTO_USUARIO", connection)
+
+        'AGREGAR DE QUE TIPO ES EL COMANDO
+        comando.CommandType = CommandType.StoredProcedure
+
+        'AGREGAR LOS VALORES A ENVIAR AL SP
+        comando.Parameters.AddWithValue("@opcion", "2")
+        comando.Parameters.AddWithValue("@usuario", usu)
+        comando.Parameters.AddWithValue("@contrasena", contra)
+        comando.Parameters.AddWithValue("@intentos", inte)
+        comando.Parameters.AddWithValue("@nivelSeg", niv)
+        comando.Parameters.AddWithValue("@id", id)
+
+        'EJECUTAR UN ADAPTADOR
+        Dim adaptador As New SqlDataAdapter(comando)
+
+        'GUARDAR EN UNA TABLA
+        Dim tabla As New DataTable
+        adaptador.Fill(tabla)
+
+
+        If tabla.Rows(0)(0).ToString() = "1" Then
+            MessageBox.Show("El dato se actualizo correctamente")
+            Mostrar(con)
+        Else MessageBox.Show("El dato no se actualizo correctamente")
+        End If
+    End Sub
+
+    Private Sub Eliminar(ByVal con As String, ByVal id As String)
+
+        'CADENA DE CONEXION
+        'Dim connectionString As String = "Data source=DESKTOP-B1JIUS0\SQLEXPRESS02; Initial Catalog=Prueba01;Integrated Security=True"
+        ' Crear una nueva conexión a SQL Server
+        Dim connection As New SqlConnection(con)
+
+        Dim comando As New SqlCommand("SP_MTTO_USUARIO", connection)
+
+        'AGREGAR DE QUE TIPO ES EL COMANDO
+        comando.CommandType = CommandType.StoredProcedure
+
+        'AGREGAR LOS VALORES A ENVIAR AL SP
+        comando.Parameters.AddWithValue("@opcion", "3")
+        comando.Parameters.AddWithValue("@id", id)
+
+        'EJECUTAR UN ADAPTADOR
+        Dim adaptador As New SqlDataAdapter(comando)
+
+        'GUARDAR EN UNA TABLA
+        Dim tabla As New DataTable
+        adaptador.Fill(tabla)
+
+
+        If tabla.Rows(0)(0).ToString() = "1" Then
+            MessageBox.Show("El dato se elimino correctamente")
+            Mostrar(con)
+        Else MessageBox.Show("El dato no se elimino correctamente")
+        End If
+    End Sub
+
+    Private Sub Limpiar()
+        txtUsu.Text = ""
+        txtCon.Text = ""
+        numInt.Value = 0
+        numNiv.Value = 0
+        btnGuardar.Enabled = True
+        btnEditar.Enabled = False
+        btnEliminar.Enabled = False
+
+    End Sub
+End Class
